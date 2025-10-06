f0b07b8a20bc59d184b3ec7dbb0f23e9226bb8be
Nycolas R. Alves <nycolasramon3@gmail.com>
2025-10-06 01:38:52 -0300

    [feat] Revisão de props e children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Footer from "@/components/Footer";
 import Menu from "@/components/Menu";
+import User from "@/components/User";
 
 const Home = () => {
+  const userName = "Nycolas";
+
   return (
     <div>
-      <Menu />
-      <br />
-      <h2>Bem-vindo Celke!</h2>
-      <br />
+      <Menu /><br />
+
+      <User name={userName}>
+        <p>
+          Este é um conteúdo extra fornecido como children.
+        </p>
+      </User><br />
+
+      <h2>Bem-vindo Celke!</h2><br />
       <Footer />
     </div>
   );
