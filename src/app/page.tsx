4b1864e616e267e5a7099987941bf0505717f7ab
Nycolas R. Alves <nycolasramon3@gmail.com>
2025-10-06 01:51:23 -0300

    [feat] Revisão de hooks e useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
+'use client'
+
 import Footer from "@/components/Footer";
 import Menu from "@/components/Menu";
 import User from "@/components/User";
+import { useState } from "react";
 
 const Home = () => {
-  const userName = "Nycolas";
+  const [nameUser, setNameUser] = useState("Nycolas") ;
 
   return (
     <div>
       <Menu /><br />
 
-      <User name={userName}>
+      <User name={nameUser}>
         <p>
           Este é um conteúdo extra fornecido como children.
         </p>
       </User><br />
+
+      <button onClick={() => setNameUser('Nycolass')}>
+        Alterar nome
+      </button>
 
       <h2>Bem-vindo Celke!</h2><br />
       <Footer />
